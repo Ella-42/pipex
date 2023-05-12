6c742c548a72682a80719e84cd81f0a2c9a3cabe
LarsCampus19 <Larsp.jobs@gmail.com>
2023-05-12 15:55:03 +0200

    (auto_push)

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 17:51:03 by lpeeters          #+#    #+#             */
-/*   Updated: 2023/05/11 21:04:06 by lpeeters         ###   ########.fr       */
+/*   Updated: 2023/05/12 15:43:16 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 char	*fetch_paths(char *envp[])
 {
-	int		i;
-
-	i = -1;
-	while (envp[i++])
-		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
-			return (envp[i] + 5);
+	while (*envp && *envp++)
+		if (ft_strncmp(*envp, "PATH=", 5) == 0)
+			return (*envp + 5);
 	err(PATH_ENV_ERR);
 	return (NULL);
 }
@@ -44,21 +41,25 @@ char	*arg_path(char *paths, char cmd[])
 
 void	child(int p_fd[], int fd[], char *av[], char *envp[])
 {
+	char	**acmd;
+
 	dup2(fd[0], 0);
 	dup2(p_fd[1], 1);
 	close(p_fd[1]);
-	if (execve(arg_path(fetch_paths(envp), ft_strtrim(av[2], " ")),
-			ft_split(av[2], ' '), envp) == -1)
+	acmd = ft_split(av[2], ' ');
+	if (execve(arg_path(fetch_paths(envp), acmd[0]), acmd, envp) < 0)
 		err(EXECVE_ERR);
 }
 
 void	parent(int p_fd[], int fd[], char *av[], char *envp[])
 {
+	char	**acmd;
+
 	dup2(fd[1], 1);
 	dup2(p_fd[0], 0);
 	close(p_fd[1]);
-	if (execve(arg_path(fetch_paths(envp), ft_strtrim(av[3], " ")),
-			ft_split(av[3], ' '), envp) == -1)
+	acmd = ft_split(av[3], ' ');
+	if (execve(arg_path(fetch_paths(envp), acmd[0]), acmd, envp) < 0)
 		err(EXECVE_ERR);
 }
 
@@ -72,13 +73,13 @@ int	main(int ac, char *av[], char *envp[])
 		err(INPUT_ERR);
 	if (pipe(p_fd) < 0)
 		err(PIPE_ERR);
-	pid = fork();
-	if (pid < 0)
-		err(FORK_ERR);
 	fd[0] = open(av[1], O_RDONLY);
 	fd[1] = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (fd[0] < 0 || fd[1] < 0)
 		err(OPEN_ERR);
+	pid = fork();
+	if (pid < 0)
+		err(FORK_ERR);
 	if (!pid)
 		child(p_fd, fd, av, envp);
 	parent(p_fd, fd, av, envp);
